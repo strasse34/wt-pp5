053abd01ea4bf8eabec2ec82ea27c9742d04a257
strasse34 <rmirzaie2021@gmail.com>
2023-11-21 11:00:31 +0000

    added followers, refactored  profile and post views to generics

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,15 +1,16 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from .models import Follower
 
-class LikeSerializer(serializers.ModelSerializer):
+class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source= 'followed.username')
 
    
     class Meta:
-        model = Like
+        model = Follower
         fields = [
-            'id', 'owner', 'post', 'created_at'
+            'id', 'owner', 'followed', 'created_at', 'followed_name'
         ]
 
     def create(self, validated_data):
