a97115b4ee1523893ad435ef64d85d218937fefb
strasse34 <rmirzaie2021@gmail.com>
2023-11-23 20:03:15 +0000

    added counts to profile

--- likes/serializers.py
+++ likes/serializers.py
@@ -2,6 +2,7 @@ from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
+
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
@@ -27,9 +29,19 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         return request.user == obj.owner
 
+    def get_like_id(self,obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+        ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title', 'content',
-             'profile_image', 'is_owner', 'profile_id', 'image', 'image_filter'
+             'profile_image', 'is_owner', 'profile_id', 'image', 'image_filter',
+             'like_id'             
         ]
